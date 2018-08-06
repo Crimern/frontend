6baa6c05c441bc0fa803f15c9e6ffd6f2084dd14
Rekseto <ReksetoAjx@gmail.com>
2018-08-06 12:16:48 +0200

    Syncing frontend with backend

--- src/redux/endpoints.js
+++ src/redux/endpoints.js
@@ -1,1 +1,5 @@
-const host = "http://localhost:3000";
+const host = "http://localhost:8000";
+
+
+export const CRIME_GENERIC = `${host}/api/crime/`;
+export const CRIME_FETCH_RADIUS = `${host}/api/crime/radius`;
