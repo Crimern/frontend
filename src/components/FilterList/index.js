0cf332dad19340933bbe482d1ec9ac26fb0f6373
Rekseto <ReksetoAjx@gmail.com>
2018-08-10 02:14:10 +0200

    FilterList component

---
+++ src/components/FilterList/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import {crimeTypeFetchRequest} from '../../redux/actions/crimeTypeActions'
+import Filter from "./filter";
+
+class FilterList extends Component {
+  componentDidMount() {
+    this.props.crimeTypesFetch()
+  }
+
+  render() {
+    const { crimeTypes } = this.props;
+    return (
+      <div>
+        <fieldset>
+        <legend>Choose your legend</legend>
+        {crimeTypes.map(item => <Filter key={item._id} name={item.name} id={item._id} />)}
+        </fieldset>
+
+      </div>
+    )
+  }
+}
+
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    crimeTypesFetch: () => dispatch(crimeTypeFetchRequest())
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    crimeTypes: state.crimeTypeStore.crimeTypes
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterList)
